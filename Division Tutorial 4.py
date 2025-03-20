fe7bc84835672071346b3ac757f4e45ef5f54196
shresthaPratik-061 <shresthapratik061virgo@gmail.com>
2025-03-20 09:35:44 +0545

    division

---
+++ Division Tutorial 4.py
@@ -0,0 +1,10 @@
+#user input
+num1 = float(input("Enter the numerator: "))
+num2 = float(input("Enter the denominator: "))
+
+#check if divisible by 0
+if num2 != 0:
+    result = num1 / num2
+    print(f"Result: {result}")
+else:
+    print("Division by zero is not allowed.")
