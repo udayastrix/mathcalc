941ce79eb78a55cead5c27255617a88a0411bd50
Ronast <ronastacharya@gmail.com>
2025-03-20 08:14:41 +0545

    this is multiplication

---
+++ # Get user input and multiply.py
@@ -0,0 +1,7 @@
+# Get user input and multiply
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Output the result
+print("The result is:", num1 * num2)
+0
